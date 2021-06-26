01e155936d5e135a21a58891df00fb3b77b34904
Ondreu <86148050+Ondreu@users.noreply.github.com>
2021-06-26 21:07:09 +0400

    Add files via upload

---
+++ Application/Program.cs
@@ -0,0 +1,31 @@
+﻿using Ninject;
+using ServiceStation;
+
+namespace ApplicationLayer
+{
+    public static class Program
+    {
+        private static int clientId;
+        private static int serviceId;
+        private static int contractId;
+        public static void Main()
+        {
+            clientId = (int)SqlExecuter.Execute("SELECT MAX(Id) AS Id FROM Clients", "Clients").Rows[0]["Id"];
+            serviceId = (int)SqlExecuter.Execute("SELECT MAX(Id) AS Id FROM Services", "Services").Rows[0]["Id"];
+            contractId = (int)SqlExecuter.Execute("SELECT MAX(Id) AS Id FROM Contract", "Contract").Rows[0]["Id"];
+        }
+        public static int GetClientId()
+        {
+            return ++clientId;
+        }
+        public static int GetServiceId()
+        {
+            return ++serviceId;
+        }
+        public static int  GetContractId()
+        {
+            return ++contractId;
+        }
+    }
+    
+}
---
+++ Application/SqlExecuter.cs
@@ -0,0 +1,91 @@
+﻿using ServiceStation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer
+{
+    public static class SqlExecuter
+    {
+        private static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\программы\\ServiceStation\\domain1\\ServiceStation\\Database.mdf;Integrated Security=True;Current Language=Russian";
+        private static SqlConnection Connection = new SqlConnection(connectionString);
+
+        public static DataTable Select(string tableName)
+        {
+            var query = "SELECT * FROM " + tableName;
+
+            Connection.Close();
+            Connection.Open();
+
+            SqlCommand command = new SqlCommand(query, Connection);
+            command.ExecuteNonQuery();
+            SqlDataAdapter dataAdp = new SqlDataAdapter(command);
+            DataTable dt = new DataTable(tableName);
+            dt.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            dataAdp.Fill(dt);
+
+            Connection.Close();
+            return dt;
+        }
+
+        public static void Insert(IItem item)
+        {
+            var sb = new StringBuilder();
+            sb.Append("INSERT INTO " + item.tableName);
+            sb.Append(" (");
+            foreach (var e in item.GetType().GetFields())
+            {
+                sb.Append(e.Name + ", ");
+            }
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append(") VALUES (");
+            foreach (var e in item.GetType().GetFields())
+            {
+                sb.Append(" '" + e.GetValue(item) + "',");
+            }
+
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append(")");
+            var command = new SqlCommand(sb.ToString(), Connection);
+            Connection.Open();
+            command.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        public static void Clear()
+        {
+            var query = "TRUNCATE TABLE Clients";
+            Connection.Open();
+            SqlCommand command = new SqlCommand(query, Connection);
+            command.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        public static DataTable Execute(string query, string tableName)
+        {
+            Connection.Open();
+
+            SqlCommand command = new SqlCommand(query, Connection);
+            command.ExecuteNonQuery();
+            SqlDataAdapter dataAdp = new SqlDataAdapter(command);
+            DataTable dt = new DataTable(tableName);
+            dt.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            dataAdp.Fill(dt);
+
+            Connection.Close();
+            return dt;
+        }
+
+        public static void Execute(string query)
+        {
+            Connection.Open();
+            SqlCommand command = new SqlCommand(query, Connection);
+            command.ExecuteNonQuery();
+            Connection.Close();
+        }
+    }
+}
